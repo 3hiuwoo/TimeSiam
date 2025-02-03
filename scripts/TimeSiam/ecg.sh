95e3a59c22c90f15ba8a50b7e40c4269566392f1
Sun Xiaoyu <yinuo42@outlook.com>
2025-02-03 23:55:31 +0800

    add ecg.sh

---
+++ scripts/TimeSiam/ecg.sh
@@ -0,0 +1,32 @@
+export CUDA_VISIBLE_DEVICES=0
+model_name=TCN
+pretrain_seq_len=300
+mask_rate=0.25
+sampling_range=6
+lineage_tokens=2
+representation_using=avg
+root_path=/root/MCP/dataset
+data_path=chapman
+
+python -u run.py \
+    --task_name timesiam \
+    --is_training 0 \
+    --root_path $root_path \
+    --data_path $data_path \
+    --model_id $data_path \
+    --model $model_name \
+    --data Traffic \
+    --features M \
+    --seq_len $pretrain_seq_len \
+    --e_layers 10 \
+    --d_layers 1 \
+    --d_model 320 \
+    --d_ff 256 \
+    --n_heads 8 \
+    --patch_len 12 \
+    --stride 12 \
+    --mask_rate $mask_rate \
+    --sampling_range $sampling_range \
+    --lineage_tokens $lineage_tokens \
+    --train_epochs 100 \
+    --batch_size 16
